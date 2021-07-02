b24bf5ae5b488bdb3cc608757898f80a2347a2b8
Poltree <76885489+Poltree@users.noreply.github.com>
2021-07-02 10:30:29 -0400

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -13,7 +13,6 @@ var firebaseConfig = {
 
 function addUser(){
     user_name = document.getElementById("user_name").value;
-    firebase.database().ref("/").child(user_name).update({
-        purpose : "adding"
-    });
+    localStorage.setItem("user_name",user_name);
+    window.location = "kwitter_room.html";
 }
